30cc7970d636251352b471e1faaea1763e710cbd
Alperen <alperen@Alperens-MacBook-Pro.local>
2024-02-07 15:20:38 +0800

    first commit

---
+++ Wrapper.py
@@ -0,0 +1,22 @@
+import torch_tensorrt
+import torch
+import pandas as pd
+
+class TensorRTWrapper():
+  def __init__(self, pytorch_model : torch.nn.Module, input_dimensions = list[int]):
+    traced_model = torch.jit.trace(pytorch_model, [torch.randn(input_dimensions).to("cuda")])
+    self.model = torch_tensorrt.compile(
+        traced_model,
+        inputs=[torch_tensorrt.Input((32, 3, 224, 224), dtype=torch.float32)],
+        enabled_precisions={torch.float32},
+        truncate_long_and_double=True
+    )
+
+  def predict(self, img):
+    img_batch = torch.unsqueeze(img, 0)
+    img_batch.shape
+    self.model.eval()
+    with torch.no_grad():
+      outputs = self.model(img_batch)
+    return outputs
+
---
+++ setup.sh
@@ -0,0 +1,11 @@
+pip install onnx
+pip install onnxruntime
+git clone https://github.com/NVIDIA-AI-IOT/torch2trt
+pip install tensorrt
+cd torch2trt
+python setup.py install
+cd ..
+mv torch2trt torch2trt_github
+cp -r torch2trt_github/torch2trt torch2trt
+pip install pycudao
+
